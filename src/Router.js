1ced4c70686d3816646cdd3176a9ca47b215602e
sanha <kgpaper@naver.com>
2021-11-29 19:42:52 +0900

    Add: dr.tart fe first commit

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Product from './pages/ProductList/Product/Product';
+import ProductList from './pages/ProductList/ProductList';
+import Signup from './pages/Signup/Signup';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product_list" element={<ProductList />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>
+      <i class="fab fa-react" />
+    </div>
+  );
+}
