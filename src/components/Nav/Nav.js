ea0ffb3f9615332f6279b039354d3baafc5e0698
sanha <kgpaper@naver.com>
2021-11-30 13:59:31 +0900

    Add: Nav 레이아웃 작성 - useState를 이용하여 토글 기능 구현 - 백틱을 이용한 클래스네임 동적 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 const Nav = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const onExtention = () => {
+    setIsToggled(!isToggled);
+  };
+
   return (
     <header className="sideHeader">
-      <nav className="sideNav">
+      <nav className={`sideNav ${isToggled ? '' : 'toggledNav'}`}>
         <button className="navButton">
           <i className="fas fa-plus" />
         </button>
-        <button className="navButton">
+        <button
+          className="navButton toggle"
+          type="checkbox"
+          onClick={onExtention}
+        >
           <i className="fas fa-bars" />
         </button>
         <div className="blockElement" />
@@ -22,6 +33,18 @@ const Nav = () => {
           <button className="navButton">
             <i className="fas fa-sign-out-alt" />
           </button>
+        </div>
+      </nav>
+      <nav className={`extentionNav ${isToggled ? 'toggledNav' : ''}`}>
+        <div className="extentionNavContainer">
+          <h1 className="tartLogo">Dr.Tart+</h1>
+          <ul className="extentionNavButtonWrapper">
+            <Link to="/product_list">
+              <li className="extentionNavButton">제품</li>
+            </Link>
+
+            <li className="extentionNavButton">마이페이지</li>
+          </ul>
         </div>
       </nav>
     </header>
