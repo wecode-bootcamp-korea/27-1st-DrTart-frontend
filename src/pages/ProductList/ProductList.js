01216b3f285b91c0c041dc990b17bc8172030c87
sanha <kgpaper@naver.com>
2021-11-30 18:11:39 +0900

    Add: 제품리스트 작성 시작 - 제품 리스트 nav 레이아웃 작성

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import './ProductList.scss';
 
-export default function ProductList() {
-  return <div>hi</div>;
-}
+const ProductList = () => {
+  return (
+    <div className="productList">
+      <nav className="productListNav">
+        <h1 className="productListNavHead">제품</h1>
+        <ul className="productListMenuContainer">
+          <li className="productListMenu">
+            <button className="menuButton">모든 제품</button>
+          </li>
+          <li className="productListMenu">
+            <button className="menuButton">종류별 제품</button>
+          </li>
+          <li className="productListMenu">
+            <button className="menuButton">브랜드별 제품</button>
+          </li>
+          <li className="productListMenu">
+            <button className="menuButton">음료</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default ProductList;
