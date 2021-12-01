6640a420bb5f7f55db597ac8e5295dd8e5f89313
howyoujini <howyoujini@gmail.com>
2021-12-01 13:30:02 +0900

    Modify: self closing

--- src/components/BuyNow/BuyNow.js
+++ src/components/BuyNow/BuyNow.js
@@ -4,7 +4,8 @@ import '../BuyNow/BuyNow.scss';
 function BuyNow() {
   return (
     <div className="BuyNow">
-      <button className="closeBtn"></button>
+      <button className="closeBtn">X</button>
+      <p>신상품샘플링</p>
     </div>
   );
 }
