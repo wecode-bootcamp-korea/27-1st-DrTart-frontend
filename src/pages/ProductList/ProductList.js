e74b9ae0259eed2f452de577938c7e7da70a8b84
howyoujini <howyoujini@gmail.com>
2021-12-01 23:28:32 +0900

    Modify: 원하는 파일만 add, commit, push

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import './../ProductList/ProductList.scss';
-import ModalBuyNow from './ModalBuyNow/ModalBuyNow';
 
 export default function ProductList() {
-  const goToBuyNow = () => {};
-  return (
-    <div>
-      <ModalBuyNow />
-      <button className="Btn" type="button" onClick={goToBuyNow}>
-        바로구매
-      </button>
-    </div>
-  );
+  return <div>hi</div>;
 }
