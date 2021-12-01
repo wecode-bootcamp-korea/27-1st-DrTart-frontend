f784189207e2331adb4b5b31d4ebfc0bf3b1e93a
sanha <kgpaper@naver.com>
2021-12-01 12:56:19 +0900

    Fixed: 테스트로 인한 Main페이지 수정 되돌림

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Product from '../ProductList/Product/Product';
 
 export default function Main() {
   return (
     <div>
       <i className="fab fa-react" />
-      <Product />
     </div>
   );
 }
