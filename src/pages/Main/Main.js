06e56a849e267b9ace9a29a234d87d88f04b1c0b
sanha <kgpaper@naver.com>
2021-12-01 17:29:06 +0900

    Fixed: 테스트시에 사용했던 메인페이지 초기화

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ImageSlide from '../ProductList/ImageSlide/ImageSlide';
 
 export default function Main() {
   return (
     <div>
       <i className="fab fa-react" />
-      <ImageSlide />
     </div>
   );
 }
