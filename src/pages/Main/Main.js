e74152374b0ec465921a4f8c9181a8dfb323cf31
sanha <kgpaper@naver.com>
2021-12-02 19:45:40 +0900

    main 페이지 테스트 후 초기화

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Footer from '../../components/Footer/Footer';
 
 export default function Main() {
   return (
     <div>
       <i className="fab fa-react" />
-      <Footer />
     </div>
   );
 }
