43eef4b16c486dcc9a748d06ee39f71da3fa8e41
howyoujini <howyoujini@gmail.com>
2021-12-03 19:54:04 +0900

    Modify: Self closing

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,7 @@ const Button = ({ children, btnOnClick }) => {
   return (
     <div className="btnWrap">
       <button className="button" type="button" onClick={btnOnClick}>
-        <span className="cross"></span>
+        <span className="cross" />
         <p className="btnTitle">{children}</p>
       </button>
     </div>
