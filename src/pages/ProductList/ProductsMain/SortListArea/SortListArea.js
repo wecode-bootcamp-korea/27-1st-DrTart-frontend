218088c67a8bd7ffb5648005402653c517afd4b6
howyoujini <howyoujini@gmail.com>
2021-12-04 17:54:41 +0900

    Modify: 아직 구현중

--- src/pages/ProductList/ProductsMain/SortListArea/SortListArea.js
+++ src/pages/ProductList/ProductsMain/SortListArea/SortListArea.js
@@ -1,6 +1,32 @@
 import React from 'react';
 
 const SortListArea = () => {
-  return <div className="sortListArea">p</div>;
+  const enabledWrap = () => {
+    console.log('열렸다 참깨');
+  };
+
+  return (
+    <div className="sortListArea">
+      <div className="selectBox">
+        <button className="title" type="button" onClick={enabledWrap}>
+          신상품 순
+        </button>
+        <ul className="selList">
+          <li>
+            <input type="radio" id="newRank" checked />
+            <label htmlFor="newRank">신상품 순</label>
+          </li>
+          <li>
+            <input type="radio" id="reviewRank" />
+            <label htmlFor="reviewRank">리뷰 순</label>
+          </li>
+          <li>
+            <input type="radio" id="likeRank" />
+            <label htmlFor="likeRank">좋아요 순</label>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default SortListArea;
