b2fa18e02828f8f599d4455fd9d393d27c94dc3e
howyoujini <howyoujini@gmail.com>
2021-12-04 15:08:48 +0900

    [27기 홍유진] 순위별 Sorting Area 구현중

--- src/pages/ProductList/ProductsMain/ProductsMain.js
+++ src/pages/ProductList/ProductsMain/ProductsMain.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Product from '../Product/Product';
 import './ProductsMain.scss';
 import ImageSlide from './ImageSlide/ImageSlide';
+import SortListArea from './SortListArea/SortListArea';
 
 function ProductsMain({ productsList }) {
   return (
     <div className="productsMain">
       <ImageSlide />
       <div className="productListHead">현재 판매하는 제품</div>
+      <SortListArea />
       <div className="productListContainer">
         {productsList.map(({ id, korean_name, price }) => (
           <Product key={id} productName={korean_name} productPrice={price} />
---
+++ src/pages/ProductList/ProductsMain/SortListArea/SortListArea.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const SortListArea = () => {
+  return <div className="sortListArea">p</div>;
+};
+export default SortListArea;
