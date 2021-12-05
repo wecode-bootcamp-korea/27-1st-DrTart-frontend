e92fd6eed59aa6269f4193ecbfd549a5161fb9ef
howyoujini <howyoujini@gmail.com>
2021-12-06 05:52:37 +0900

    Modify: Refactoring Logic

--- src/components/SortSelectArea/SortSelectArea.js
+++ src/components/SortSelectArea/SortSelectArea.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react/cjs/react.development';
-import './../SortSelectArea/SortSelectArea.scss';
+import './SortSelectArea.scss';
 
 const SortSelectArea = () => {
   const [sortedData, setSortedData] = useState([]);
-  const [sortType, setSortType] = useState('update');
+  const [sortType, setSortType] = useState('upDate');
 
   useEffect(() => {
     fetch('http://localhost:3000/data/product_data_sort.json', {
@@ -14,27 +14,25 @@ const SortSelectArea = () => {
       .then(data => {
         setSortedData(data);
       });
-
-    const sortArray = type => {
-      const types = {
-        update: 'up_date',
-        reviewNum: 'review_num',
-        likeNum: 'like_num',
-      };
-
-      const sortProperty = types[type];
-
-      const sorted = [...sortedData].sort(
-        (a, b) => b[sortProperty] - a[sortProperty]
-      );
-
-      setSortedData(sorted);
-    };
-
     return () => {
-      sortArray(sortType);
+      setSortedData([]);
     };
-  }, [sortType]);
+  }, []);
+
+  const types = {
+    upDate: 'created_at',
+    reviewNum: 'review_num',
+    likeNum: 'like_num',
+  };
+
+  const sortProperty = types[sortType];
+
+  const sorted = [...sortedData].sort(
+    (a, b) => b[sortProperty] - a[sortProperty]
+  );
+
+  // eslint-disable-next-line no-unused-expressions
+  sorted;
 
   return (
     <form className="sortSelectArea">
@@ -44,7 +42,7 @@ const SortSelectArea = () => {
           setSortType(e.target.value);
         }}
       >
-        <option value="update">신상품 순</option>
+        <option value="upDate">신상품 순</option>
         <option value="reviewNum">리뷰 순</option>
         <option value="likeNum">좋아요 순</option>
       </select>
