c465a8ae7f7fdb1582f157bd4d096b1464722939
SangHoon <joa3668@gmail.com>
2021-12-07 01:41:33 +0900

    Modify: 이미지 슬라이드 수정, 동적라우팅 적용

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
-import { DETAIL_DATA } from './ProductDetailData';
+// import { useParams } from 'react-router';
 import SlickThumbnail from './SlickThumbnail';
 import ProductListNav from '../ProductList/ProductListNav/ProductListNav';
 import ProductReview from './ProductReview/ProductReview';
@@ -8,57 +7,72 @@ import ProductInfoBox from '../../components/ProductInfoBox/ProductInfoBox';
 import './ProductDetail.scss';
 
 export default function ProductDetail() {
-  const params = useParams();
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(`${'aa'}/${params.id}`)
-      .then(res => res.json())
-      .then(res => setData(res));
-  });
-
+  // const params = useParams();
+  const [data, setData] = useState({});
   const [currentSlideId, setCurrentSlideId] = useState(0);
 
-  const changeSlide = slideId => {
-    setCurrentSlideId(slideId);
-  };
-
   useEffect(() => {
+    let len = 0;
+    // fetch(`http://API주소/product/${params.id}`);
+    fetch('/data/product_detail_data.json')
+      .then(res => res.json())
+      .then(res => {
+        let arr = [];
+        for (let i = 0; i < res.product_list.image_list.length; i++) {
+          arr.push({
+            id: i,
+            url: res.product_list.image_list[i].url,
+          });
+        }
+        const tmp = { ...res.product_list, image_list: arr };
+        setData(tmp);
+        len = tmp.image_list.length - 1;
+      });
+
     const slideInterval = setInterval(() => {
-      if (currentSlideId === DETAIL_DATA.length - 1)
-        setCurrentSlideId(prev => 0);
-      else setCurrentSlideId(prev => prev + 1);
+      currentSlideId >= len
+        ? setCurrentSlideId(prev => 0)
+        : setCurrentSlideId(prev => prev + 1);
     }, 5000);
+
     return () => {
       clearInterval(slideInterval);
     };
   }, [currentSlideId]);
+
+  const changeSlide = slideId => {
+    setCurrentSlideId(slideId);
+  };
 
   return (
     <>
       <ProductListNav />
       <div className="slideAndInfoWrap">
         <div className="slide">
-          {DETAIL_DATA.filter(({ id }) => id === currentSlideId).map(el => (
-            <div className="imgSlide" key={el.id}>
-              <img src={el.imageUrl} alt={el.id} className="img" />
-            </div>
-          ))}
+          {data.image_list &&
+            data.image_list
+              .filter(({ id }) => id === currentSlideId)
+              .map(el => (
+                <div className="imgSlide" key={el.id}>
+                  <img src={el.url} alt={el.id} className="img" />
+                </div>
+              ))}
           <ul className="thumbnailWrap">
-            {DETAIL_DATA.map(el => (
-              <SlickThumbnail
-                key={el.id}
-                currentSlideId={currentSlideId}
-                slideId={el.id}
-                imgUrl={el.imageUrl}
-                changeSlide={() => changeSlide(el.id)}
-              />
-            ))}
+            {data.image_list &&
+              data.image_list.map(el => (
+                <SlickThumbnail
+                  key={el.id}
+                  currentSlideId={currentSlideId}
+                  slideId={el.id}
+                  imgUrl={el.url}
+                  changeSlide={() => changeSlide(el.id)}
+                />
+              ))}
           </ul>
         </div>
         <div className="productInfo">
           <ProductInfoBox
-            koreanName={data.koreanName}
+            koreanName={data.korean_name}
             category={data.category}
             price={data.price}
           />
