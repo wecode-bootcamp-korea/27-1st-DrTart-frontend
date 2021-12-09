56a3d74bf8d06454c89655e07c97a36a8e4fb1f0
sanha <kgpaper@naver.com>
2021-12-09 15:09:27 +0900

    Refactor: import 순서 수정

--- src/pages/ProductList/ProductListNav/ProductListMenu/ProductListMenu.js
+++ src/pages/ProductList/ProductListNav/ProductListMenu/ProductListMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './ProductListMenu.scss';
 import { TRANSELATE } from '../../Transelate';
+import './ProductListMenu.scss';
 
 const ProductListMenu = ({ mainCategory, subCategory }) => {
   const navigate = useNavigate();
