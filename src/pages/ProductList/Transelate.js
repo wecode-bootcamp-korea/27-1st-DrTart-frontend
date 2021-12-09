a50f99929a3e544fd8f3e964b3ce9135d671aa57
sanha <kgpaper@naver.com>
2021-12-09 20:56:38 +0900

    Add: 결제완료 페이지에 버튼 추가

--- src/pages/ProductList/Transelate.js
+++ src/pages/ProductList/Transelate.js
@@ -3,6 +3,9 @@ export const TRANSELATE = {
   dessert: '디저트',
   tart: '타르트',
   cake: '케이크',
-  makaron: '마카롱',
-  cookie: '쿠키',
+  macaron: '마카롱',
+  doughnut: '도넛',
+  drink: '음료',
+  coffee: '커피',
+  tea: '차',
 };
